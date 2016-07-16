d7a190bf6dd685b3be6937a54d9d4456d8a2f3bc
jeffreyca16@gmail.com <jeffreyca16@gmail.com>
2016-07-16 15:46:35 -0400

    Fix toolbar and implement dummy overflow menu
    Former-commit-id: 4939e03883baa0a79422a85f419616bd0c1d772a

--- app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
+++ app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
@@ -3,7 +3,12 @@ package jeffrey.astronomypictureofthedaynasa;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.showOverflowMenu();
+        setSupportActionBar(myToolbar);
 
         date = (TextView) findViewById(R.id.date);
         today = current_date = dateToString();
@@ -105,5 +113,24 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // editNote(info.id);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
