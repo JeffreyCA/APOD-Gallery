816336e40ee137db4307f2f8b91124d8169e23c8
jeffreyca16@gmail.com <jeffreyca16@gmail.com>
2016-07-16 14:21:26 -0400

    Design prototype
    Former-commit-id: faf09971cf0b8ed9abb7fe7240bcca24d12a9e13

--- app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
+++ app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_design1);
+        setContentView(R.layout.activity_main);
     }
 }
