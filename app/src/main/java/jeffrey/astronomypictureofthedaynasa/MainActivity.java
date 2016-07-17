554815d6dc468bf54bbad110fec8e21b7d498fd8
jeffreyca16@gmail.com <jeffreyca16@gmail.com>
2016-07-17 10:45:17 -0400

    Fix FAB behaviour
    Former-commit-id: b2be9a028065bfc1f556587cb15f252786aa7edd

--- app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
+++ app/src/main/java/jeffrey/astronomypictureofthedaynasa/MainActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.sothree.slidinguppanel.FloatingActionButtonLayout;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import java.text.ParseException;
@@ -21,10 +24,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    final private String API_KEY = "***REMOVED***";
     String today;
     String current_date;
     TextView date;
-    final private String API_KEY = "***REMOVED***";
+    FloatingActionButton fab;
+    FloatingActionButtonLayout fab_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.showOverflowMenu();
         setSupportActionBar(myToolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab_layout = (FloatingActionButtonLayout) findViewById(R.id.fab_layout);
 
         ImageView imageView = (ImageView) findViewById(R.id.image);
         Glide.with(this).load("http://apod.nasa.gov/apod/image/1607/ayiomamitis-star-trails-marathon-oinoe-2016.jpg").into(imageView);
@@ -65,23 +73,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final String TAG = "LISTENER";
         SlidingUpPanelLayout sliding_panel = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        sliding_panel.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
-            FloatingActionButton button = (FloatingActionButton) findViewById(R.id.fab);
+
+        sliding_panel.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            FloatingActionButton fabe = (FloatingActionButton) findViewById(R.id.fab);
+
+            @Override
+            public void onPanelExpanded(View panel) {
+                Log.i(TAG, "onPanelExpanded");
+                fabe.hide();
+            }
 
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
-                button.hide();
+
             }
 
             @Override
-            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
-                if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED)
-                    button.show();
+            public void onPanelCollapsed(View panel) {
+                Log.i(TAG, "onPanelCollapsed");
+                fabe.show();
+            }
+
+            @Override
+            public void onPanelAnchored(View panel) {
+                Log.i(TAG, "onPanelAnchored");
+            }
+
+            @Override
+            public void onPanelHidden(View panel) {
+                Log.i(TAG, "onPanelHidden");
+            }
+
+            @Override
+            public void onPanelHiddenExecuted(View panel, Interpolator interpolator, int duration) {
+                Log.i(TAG, "onPanelHiddenExecuted");
+            }
+
+            @Override
+            public void onPanelShownExecuted(View panel, Interpolator interpolator, int duration) {
+                Log.i(TAG, "onPanelShownExecuted");
+            }
+
+            @Override
+            public void onPanelExpandedStateY(View panel, boolean reached) {
+                Log.i(TAG, "onPanelExpandedStateY");
+            }
+
+            @Override
+            public void onPanelCollapsedStateY(View panel, boolean reached) {
+                Log.i(TAG, "onPanelCollapsedStateY");
+            }
+
+            @Override
+            public void onPanelLayout(View panel, SlidingUpPanelLayout.PanelState state) {
+                Log.i(TAG, "onPanelLayout");
             }
         });
-
-
     }
 
     private String dateToString() {
@@ -140,4 +189,14 @@ public class MainActivity extends AppCompatActivity {
                 return super.onContextItemSelected(item);
         }
     }
+
+	/*@Override
+	public void onBackPressed() {
+        if (mLayout != null &&
+                (mLayout.getPanelState() == PanelState.EXPANDED || mLayout.getPanelState() == PanelState.ANCHORED)) {
+            mLayout.setPanelState(PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
+    }*/
 }
