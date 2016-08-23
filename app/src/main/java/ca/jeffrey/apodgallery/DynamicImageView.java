914629d08e69dd138af516ff29a5de9951532d14
Jeffrey <jeffreyca16@gmail.com>
2016-08-22 21:08:07 -0400

    Change the way image is displayed to show more of the content
    Former-commit-id: af82c41514b23cb1fc33650326098694c6517f0b

---
+++ app/src/main/java/ca/jeffrey/apodgallery/DynamicImageView.java
@@ -0,0 +1,31 @@
+package ca.jeffrey.apodgallery;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * http://stackoverflow.com/questions/13992535/android-imageview-scale-smaller-image-to-width-with-flexible-height-without-crop
+ */
+public class DynamicImageView extends ImageView {
+
+    public DynamicImageView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+        final Drawable d = this.getDrawable();
+
+        if (d != null) {
+            // ceil not round - avoid thin vertical gaps along the left/right edges
+            final int width = View.MeasureSpec.getSize(widthMeasureSpec);
+            final int height = (int) Math.ceil(width * (float) d.getIntrinsicHeight() / d.getIntrinsicWidth());
+            this.setMeasuredDimension(width, height);
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
