151a82fc2a0bdc6c0ccb5c4416574cd8085cb402
Jeffrey <jeffreyca16@gmail.com>
2016-08-22 21:13:52 -0400

    Second version of improved ImageView
    Former-commit-id: d886e6aa36d9d65a1557b23b637af6bbf20be5fd

---
+++ app/src/main/java/ca/jeffrey/apodgallery/ProportionalImageView.java
@@ -0,0 +1,35 @@
+package ca.jeffrey.apodgallery;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * http://www.ryadel.com/en/android-proportionally-stretch-imageview-fit-whole-screen-width-maintaining-aspect-ratio/
+ */
+public class ProportionalImageView extends ImageView {
+
+    public ProportionalImageView(Context context) {
+        super(context);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProportionalImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable d = getDrawable();
+        if (d != null) {
+            int w = MeasureSpec.getSize(widthMeasureSpec);
+            int h = w * d.getIntrinsicHeight() / d.getIntrinsicWidth();
+            setMeasuredDimension(w, h);
+        }
+        else super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
