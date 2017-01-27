166369528a3e78549a512224f4611cd51d82e34f
Jeffrey <jeffreyca16@gmail.com>
2017-01-26 23:54:49 -0500

    Retrieve images from external storage for use in widget
    Former-commit-id: ecb981fa1731dd71e2ccbf20adfa355650233c1e

--- app/src/main/java/ca/jeffrey/apodgallery/widget/StackWidgetService.java
+++ app/src/main/java/ca/jeffrey/apodgallery/widget/StackWidgetService.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -26,8 +30,8 @@ public class StackWidgetService extends RemoteViewsService {
     }
 }
 class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
-    private static final int mCount = 10;
-    private List<WidgetItem> mWidgetItems = new ArrayList<WidgetItem>();
+    private int mCount = 10;
+    private List<WidgetItem> mWidgetItems = new ArrayList<>();
     private Context mContext;
     private int mAppWidgetId;
 
@@ -36,22 +40,50 @@ class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
         mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
                 AppWidgetManager.INVALID_APPWIDGET_ID);
     }
+
+    public int countFiles() {
+        // Replace with preferences value
+        File file=new File(Environment.getExternalStorageDirectory().getPath() + "APOD");
+        File[] list = file.listFiles();
+
+        mCount = list.length;
+        for (File f: list){
+            String name = f.getName();
+            if (name.endsWith(".jpg"));
+        }
+        return 0;
+
+    }
     public void onCreate() {
         Log.i("ONCREATE", "created");
         Calendar cal = GregorianCalendar.getInstance();
         Date date = cal.getTime();
         SimpleDateFormat shortDateFormat = new SimpleDateFormat("MMM dd yyyy");
+        Log.i("PATH", Environment.getExternalStorageDirectory().getPath());
+        // Replace with preferences value
+        File file=new File(Environment.getExternalStorageDirectory().getPath() + "/APOD/");
+        File[] list = file.listFiles();
+
+        mCount = list.length;
+
+        for (File f: list){
+            String name = f.getName();
+            if (name.endsWith(".jpg")) {
+                name = name.replaceAll(".jpg", "");
+                mWidgetItems.add(new WidgetItem(name));
+            }
+        }
 
         // In onCreate() you setup any connections / cursors to your data source. Heavy lifting,
         // for example downloading or creating content etc, should be deferred to onDataSetChanged()
         // or getViewAt(). Taking more than 20 seconds in this call will result in an ANR.
-        for (int i = 0; i < mCount; i++) {
-            String day = shortDateFormat.format(date);
-            Log.i("DATE", day);
-            mWidgetItems.add(new WidgetItem(day));
-            cal.add(Calendar.DAY_OF_YEAR, -1);
-            date = cal.getTime();
-        }
+        // for (int i = 0; i < mCount; i++) {
+        //     String day = shortDateFormat.format(date);
+        //     Log.i("DATE", day);
+        //     mWidgetItems.add(new WidgetItem(day));
+        //     cal.add(Calendar.DAY_OF_YEAR, -1);
+        //     date = cal.getTime();
+        // }
         // We sleep for 3 seconds here to show how the empty view appears in the interim.
         // The empty view is set in the StackWidgetProvider and should be a sibling of the
         // collection view.
@@ -69,14 +101,26 @@ class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
     public int getCount() {
         return mCount;
     }
+
     public RemoteViews getViewAt(int position) {
+
         // position will always range from 0 to getCount() - 1.
         // We construct a remote views item based on our widget item xml file, and set the
         // text based on the position.
         RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.layout_item);
-        rv.setTextViewText(R.id.widget_text, mWidgetItems.get(position).text);
-        Bitmap a = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.stars);
-        rv.setImageViewBitmap(R.id.stackWidgetItemPicture, a);
+        rv.setTextViewText(R.id.widget_text, mWidgetItems.get(position).getFormattedDate());
+
+        final String IMAGE_DIRECTORY = "/sdcard/APOD/";
+        final String EXT = ".jpg";
+
+        try {
+            File f = new File(IMAGE_DIRECTORY + mWidgetItems.get(position).getDate() + EXT);
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            rv.setImageViewBitmap(R.id.stackWidgetItemPicture, b);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         // Next, we set a fill-intent which will be used to fill-in the pending intent template
         // which is set on the collection view in StackWidgetProvider.
--- app/src/main/java/ca/jeffrey/apodgallery/widget/WidgetItem.java
+++ app/src/main/java/ca/jeffrey/apodgallery/widget/WidgetItem.java
@@ -1,12 +1,35 @@
 package ca.jeffrey.apodgallery.widget;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by jeffrey on 2017-01-20.
  */
 
 public class WidgetItem {
-    public String text;
-    public WidgetItem(String text) {
-        this.text = text;
+    private String date;
+
+    public WidgetItem(String date) {
+        this.date = date;
+    }
+
+    public String getFormattedDate() {
+        SimpleDateFormat fileNameFormat = new SimpleDateFormat("y-MM-dd");
+        SimpleDateFormat shortDateFormat = new SimpleDateFormat("MMM dd yyyy");
+        String formattedDate = "";
+
+        try {
+            formattedDate = shortDateFormat.format(fileNameFormat.parse(date));
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return formattedDate;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
