dcb34907498f7555c948ba4547deee8a192400bb
Jeffrey <jeffreyca16@gmail.com>
2017-06-16 15:09:22 -0400

    Add Changelog
    Former-commit-id: 4c047a999ac5e854306f38d3170054e4694b6801

--- app/src/main/java/ca/jeffrey/apodgallery/SettingsActivity.java
+++ app/src/main/java/ca/jeffrey/apodgallery/SettingsActivity.java
@@ -276,7 +276,7 @@ public class SettingsActivity extends Activity implements SharedPreferences
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
             builder.setTitle(getString(R.string.dialog_whats_new_title) + BuildConfig.VERSION_NAME)
-                    .setMessage(R.string.change_2_0_6)
+                    .setMessage(R.string.change_2_0_7)
                     .setPositiveButton(R.string.label_dismiss, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
